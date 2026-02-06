44add0c08a3f2db6bd7a9e2b022d802a435c1adf
MrEntenadorPokemon <andresiqueiros29@gmail.com>
2026-02-05 20:35:07 -0700

    Ejercicio restaurante (09 / Feb / 2026)

--- app.py
+++ app.py
@@ -6,12 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    # Abrimos y leemos el archivo JSON
     ruta_json = os.path.join(app.root_path, 'platillos.json')
     with open(ruta_json, 'r', encoding='utf-8') as f:
         datos_menu = json.load(f)
 
-    # Pasamos los datos al HTML
     return render_template('index.html', menu=datos_menu)
 
 
